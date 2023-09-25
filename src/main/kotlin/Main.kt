47eea8c6769d632233abb91d5469e49b899c1553
Denis Frolkov <deniska-frolkov@mail.ru>
2023-09-25 20:18:51 +0300

    Started making algorithms

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,21 @@
 import game.creature.Monster
 import game.creature.Player
-import kotlin.random.Random
 
 
 fun main() {
-    val player = Player("Player", 10,10, 20, 30)
-    val monster = Monster("Monster", 10,21, 20, 50)
+    val player = Player("Player", 30,6, 20, 30)
+    val monster = Monster("Monster", 30,6, 20, 30)
 
     println(player.attackMessage())
     println(monster.attackMessage())
+    println()
     // Пример боя
     while (player.healthPoints > 0 && monster.healthPoints > 0) {
-        val playerDamage = player.damageRange(player.range)
-        monster.damageTaking(playerDamage)
-        println("${monster.name} получил урон - $playerDamage от ${player.name}. Здоровье монстра: ${monster.healthPoints}")
-        val randomMonster = Random.nextInt(player.range)
-        player.damageTaking(randomMonster)
-        println("${monster.name} получил урон - $randomMonster от ${monster.name}. Здоровье игрока: ${player.healthPoints}")
+        monster.damageTaking(player)
+        player.damageTaking(monster)
         if (player.healthPoints < 10){
             player.regeneration(player.healthPoints)
+            println("Здоровье ${player.name} меньше 10, он воспользовался регенирацией")
         }
     }
     when {
--- src/main/kotlin/game/Creature.kt
+++ src/main/kotlin/game/Creature.kt
@@ -8,14 +8,25 @@ open class Creature(val name: String, val attack: Int, var range: Int, val defen
         require(healthPoints >= 0) { "Здоровье отрицательное, вы уже мертвы" }
     }
 
-    fun damageRange(range: Int): Int {
-        return Random.nextInt(range)
-    }
-    fun damageTaking(range: Int){
-        healthPoints -= range
-        if (healthPoints < 0){
-            healthPoints = 0
+
+    fun damageTaking(attacker: Creature) {
+        val attackModifier = calculateAttackModifier(attacker.range)
+        val successfulHit = rollDice(attackModifier)
+        if (successfulHit) {
+            val damage = Random.nextInt(attacker.range) // Произвольное значение из параметра Урон атакующего
+            println("$name получил урон - $damage от атаки. Здоровье $name: $healthPoints")
+            healthPoints -= damage
+            if (healthPoints < 0) {
+                healthPoints = 0
+            }
+        } else {
+            println("Атака ${attacker.name} не удалась против $name.")
         }
+    }
+
+    private fun calculateAttackModifier(range: Int): Int {
+        val attackModifier = (range - defense + 1)
+        return if (attackModifier < 1) 1 else attackModifier
     }
 
     fun regeneration(healthPoints: Int){
@@ -30,6 +41,18 @@ open class Creature(val name: String, val attack: Int, var range: Int, val defen
             healingCount++
         }
     }
+
+    fun rollDice(numDice: Int): Boolean {
+        val random = Random
+        for (i in 1..numDice) {
+            val roll = random.nextInt(6) + 1 // Бросаем кубик с цифрами от 1 до 6
+            if (roll == 5 || roll == 6) {
+                return true // Удар считается успешным при выпадении 5 или 6 на кубике
+            }
+        }
+        return false // Если не выпало 5 или 6 на всех кубиках, удар не успешен
+    }
+
 
     fun isDead(): Boolean {
         return healthPoints == 0
